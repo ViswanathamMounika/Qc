9c651798b15869ae5b73bde53ebb998434023c49
ViswanathamMounika <mounikav.199@gmail.com>
2023-04-03 12:45:09 +0530

    mapper testing added

---
+++ TestProjectMapper/Mapper.cs
@@ -0,0 +1,32 @@
+using BussinessLogic;
+using PAE = FluentAPI.Entities;
+using Models;
+using BussinessLogic;
+using NUnit.Framework;
+
+namespace PhysicianAvailability_Testing_Mapper
+{
+    [TestFixture]
+    public class Tests
+    {
+        // entity to model
+
+        [Test]
+        public void Test1()
+        {
+            PAE.PhysicianAvailablity physician = new PAE.PhysicianAvailablity();
+            var model = Mapper.PhyMap(physician);
+            Assert.AreEqual(model.GetType(), typeof(Physicianavailability));
+        }
+
+        // model to entity
+
+        [Test]
+        public void Test2()
+        {
+            Physicianavailability physician = new Physicianavailability();
+            var model = Mapper.PhyMap(physician);
+            Assert.AreEqual(model.GetType(), typeof(PAE.PhysicianAvailablity));
+        }
+    }
+}
